ce56702950078a4a915eb91d21f1082582219158
nikulina.svetlana <svetlana.nikulina@devexpress.com>
2026-02-12 10:06:55 +0100

    upd

---
+++ CS/EF/OpenViewFromDashboardEF/OpenViewFromDashboardEF.Blazor.Server/Controllers/BlazorShowDetailViewFromDashboardController.cs
@@ -0,0 +1,40 @@
+﻿using DevExpress.ExpressApp.Blazor;
+using DevExpress.ExpressApp;
+using DevExpress.Persistent.Base;
+using Microsoft.JSInterop;
+using static System.Net.Mime.MediaTypeNames;
+using dxTestSolution.Module.BusinessObjects;
+
+namespace OpenViewFromDashboard.Blazor.Server.Controllers;
+public class BlazorShowDetailViewFromDashboardController : ObjectViewController<DetailView, IDashboardData> {
+    private IJSRuntime JSRuntime { get; set; }
+    private DotNetObjectReference<BlazorShowDetailViewFromDashboardController> controllerReference;
+    public BlazorShowDetailViewFromDashboardController() {
+        controllerReference = DotNetObjectReference.Create(this);
+    }
+    protected override void OnActivated() {
+        base.OnActivated();
+        var application = (BlazorApplication)Application;
+        JSRuntime = application.ServiceProvider.GetRequiredService<IJSRuntime>();
+        _ = JSRuntime.InvokeVoidAsync("customScript.registerController", controllerReference).Preserve();
+    }
+    protected override void Dispose(bool disposing) {
+        base.Dispose(disposing);
+        controllerReference.Dispose();
+    }
+    [JSInvokable]
+    public void ShowDetailView(string oidString) {
+        if (!Guid.TryParse(oidString, out var oid)) {
+            return;
+        }
+        var objectSpace = Application.CreateObjectSpace(typeof(Contact));
+        var item = objectSpace.FirstOrDefault<Contact>(c => c.ID == oid);
+        if (item is not null) {
+            var detailView = Application.CreateDetailView(objectSpace, item);
+            Frame.SetView(detailView);
+        } else {
+            objectSpace.Dispose();
+        }
+    }
+}
+
---
+++ CS/EF/OpenViewFromDashboardEF/OpenViewFromDashboardEF.Module/BusinessObjects/OpenViewFromDashboardEFDbContext.cs
@@ -0,0 +1,47 @@
+﻿using DevExpress.ExpressApp.EFCore.Updating;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using DevExpress.Persistent.BaseImpl.EF.PermissionPolicy;
+using DevExpress.Persistent.BaseImpl.EF;
+using DevExpress.ExpressApp.Design;
+using DevExpress.ExpressApp.EFCore.DesignTime;
+using dxTestSolution.Module.BusinessObjects;
+
+namespace OpenViewFromDashboardEF.Module.BusinessObjects;
+
+// This code allows our Model Editor to get relevant EF Core metadata at design time.
+// For details, please refer to https://supportcenter.devexpress.com/ticket/details/t933891.
+public class OpenViewFromDashboardEFContextInitializer : DbContextTypesInfoInitializerBase {
+	protected override DbContext CreateDbContext() {
+		var optionsBuilder = new DbContextOptionsBuilder<OpenViewFromDashboardEFEFCoreDbContext>()
+            .UseSqlServer(";")
+            .UseChangeTrackingProxies()
+            .UseObjectSpaceLinkProxies();
+        return new OpenViewFromDashboardEFEFCoreDbContext(optionsBuilder.Options);
+	}
+}
+//This factory creates DbContext for design-time services. For example, it is required for database migration.
+public class OpenViewFromDashboardEFDesignTimeDbContextFactory : IDesignTimeDbContextFactory<OpenViewFromDashboardEFEFCoreDbContext> {
+	public OpenViewFromDashboardEFEFCoreDbContext CreateDbContext(string[] args) {
+		throw new InvalidOperationException("Make sure that the database connection string and connection provider are correct. After that, uncomment the code below and remove this exception.");
+		//var optionsBuilder = new DbContextOptionsBuilder<OpenViewFromDashboardEFEFCoreDbContext>();
+		//optionsBuilder.UseSqlServer("Integrated Security=SSPI;Data Source=(localdb)\\mssqllocaldb;Initial Catalog=OpenViewFromDashboardEF");
+        //optionsBuilder.UseChangeTrackingProxies();
+        //optionsBuilder.UseObjectSpaceLinkProxies();
+		//return new OpenViewFromDashboardEFEFCoreDbContext(optionsBuilder.Options);
+	}
+}
+[TypesInfoInitializer(typeof(OpenViewFromDashboardEFContextInitializer))]
+public class OpenViewFromDashboardEFEFCoreDbContext : DbContext {
+	public OpenViewFromDashboardEFEFCoreDbContext(DbContextOptions<OpenViewFromDashboardEFEFCoreDbContext> options) : base(options) {
+	}
+	//public DbSet<ModuleInfo> ModulesInfo { get; set; }
+	public DbSet<DashboardData> DashboardData { get; set; }
+    public DbSet<MyTask> MyTasks { get; set; }
+    public DbSet<Contact> Contacts { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.HasChangeTrackingStrategy(ChangeTrackingStrategy.ChangingAndChangedNotificationsWithOriginalValues);
+        modelBuilder.UsePropertyAccessMode(PropertyAccessMode.PreferFieldDuringConstruction);
+    }
+}
